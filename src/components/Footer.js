6d36b2e2e83f141db841b8cfb037b2cac90d8536
kespinak <kespinak@gmail.com>
2022-12-08 13:01:57 -0800

    finished adding a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './FooterStyles.css';
+import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaLinkedin, FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-container'>
+        <div className='left'/>
+          <div className='location'>
+            <FaHome size={20} style={{color: '#fff', marginRight: '2rem'}}/>
+            <div>
+              <p> address1 </p>
+              <p> address2 </p>
+            </div>
+          </div>
+          <div className='phone'>
+            <h4>
+              <FaPhone size={20} style={{color: '#fff', marginRight: '2rem'}}/>
+              760-000-0000
+            </h4>
+          </div>
+          <div className='email'>
+            <h4>
+              <FaMailBulk size={20} style={{color: '#fff', marginRight: '2rem'}}/>
+              info@gmail.com
+            </h4>
+          </div>
+
+          <div className='right' >
+            <h4>About the company</h4>
+            <p> This is me, Kevin Espina. I enjoy discussing new projects and design challenges. </p>
+            <div className='social'>
+              <FaFacebookF size={30} style={{color: '#fff', marginRight: '1rem'}}/>
+              <FaTwitter size={30} style={{color: '#fff', marginRight: '1rem'}}/>
+              <FaLinkedinIn size={30} style={{color: '#fff', marginRight: '1rem'}}/>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HomeBackground from '../components/Home-bg'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <HomeBackground />
+        <Footer />
     </div>
   )
 }
