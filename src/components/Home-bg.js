7bc44218b7b6af36ee81f1f69e9bcecb6ec47a10
kespinak <kespinak@gmail.com>
2022-12-08 12:31:01 -0800

    half way done wit home page (added background & buttons)

---
+++ src/components/Home-bg.js
@@ -0,0 +1,32 @@
+
+import React from 'react'
+import './HomeBgStyles.css';
+import imgHomeBg from '../assets/home-bg.jpg';
+import { Link } from 'react-router-dom';
+
+// import laptopImg from "../../Assets/about.png";
+// import homeLogo from '../../Assets/home-main.svg';
+// <img src={homeLogo} alt="home pic" className="img-fluid" />
+// <img src={laptopImg} alt="about" className="img-fluid" />
+
+
+
+const HomeBackground = () => {
+  return (
+    <div className='HomeBg'>
+      <div className='mask'>
+        <img className='img-fluid' src={imgHomeBg} alt="homePic" />
+      </div>
+      <div className='content'>
+        <p>Hi, I'm a FullStack Developer</p>
+        <p>React Developer</p>
+        <div>
+          <Link to='/project' className='btn'>Projects </Link>
+          <Link to='/contact' className='btn btn-light'>Contact </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeBackground;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HomeBackground from '../components/Home-bg'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <HomeBackground />
     </div>
   )
 }
