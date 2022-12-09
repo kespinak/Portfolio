e71c5f630df9e05b232a6c0674b2891db0c8771d
kespinak <kespinak@gmail.com>
2022-12-08 17:33:55 -0800

    added first project card onto projects page

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -6,17 +6,22 @@ import { NavLink } from 'react-router-dom';
 const WorkCard = () => {
   return (
     <div className='work-container'>
-      <h1 className='project-heading'>Projects</h1>
+      <h1 className='project-heading'>My Recent <strong className="purple"> Works </strong> </h1>
+      <p>Here are a few project's I've worked on recently.</p>
       <div className='project-container'>
         <div className='project-card'>
           <img src={taskify} alt='taskifyImage' />
           <h2 className='project-title'>Taskify</h2>
           <div className='taskify-description'>
-            <p> Taskify is a task management application that allows users to create, edit, and delete tasks. </p>
+            <p> Taskify is a task management application that allows users to create, edit, and delete tasks.
+            <br />
+            <br />
+            TechStack: React, Nodejs, Express, and MongoDB. 
+            </p>
           </div>
           <div className='taskify-btn' >
             <NavLink to='google.com' className='btn'>View Project</NavLink>
-            <NavLink to='google.com' className='btn'>View Repository</NavLink>
+            <NavLink to='https://github.com/kespinak/Bug-Tracker' className='btn btn-light'>View Repository</NavLink>
           </div>
         </div>
       </div>
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -10,13 +10,7 @@ const Projects = () => {
       <Navbar />
       <WorkCard />
       <div heading='PROJECTS'>
-        <h1 className="project-heading">
-          {/* * CLASSNAME='PURPLE' ISNT WORKING */}
-          My Recent <strong className="purple"> Works </strong> 
-        </h1>
-        <p style={{color: 'white'}}>
-          Here are a few projects I've worked on recently.
-        </p>
+        <h1 className="project-heading"> </h1>
       </div>
       <Footer />
     </div>
